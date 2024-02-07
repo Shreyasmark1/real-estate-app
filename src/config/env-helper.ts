9629fcdfd2c7f4f74f1c4d4afbf99263dce47c2f
Shreyas <117585934+Shreyasmark1@users.noreply.github.com>
2024-02-08 01:39:24 +0530

    env fix

--- src/config/env-helper.ts
+++ src/config/env-helper.ts
@@ -3,4 +3,4 @@ export const LS_ACESS_TOKEN_KEY = "KJGJgkgJFhEhfd"
 export const LS_REFRESH_TOKEN_KEY = "jGJHGTHHgHfdgf"
 export const LS_TRANSACTION_TOKEN_KEY = "ftfqFjfKJjkj"
 export const LS_USER_TYPE = "uedhuHWI"
-export const BASE_URL = process.env.BASE_URL
+export const BASE_URL = import.meta.env.VITE_BASE_URL
