1f2378ec23194794a9900b8a05e9749057857d3f
Shreyas <117585934+Shreyasmark1@users.noreply.github.com>
2024-02-10 16:15:37 +0530

    changed base url

--- src/config/env-helper.ts
+++ src/config/env-helper.ts
@@ -3,5 +3,5 @@ export const LS_ACESS_TOKEN_KEY = "KJGJgkgJFhEhfd"
 export const LS_REFRESH_TOKEN_KEY = "jGJHGTHHgHfdgf"
 export const LS_TRANSACTION_TOKEN_KEY = "ftfqFjfKJjkj"
 export const LS_USER_TYPE = "uedhuHWI"
-// export const BASE_URL = import.meta.env.VITE_BASE_URL
-export const BASE_URL = "http://localhost:8080/"
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+// export const BASE_URL = "http://localhost:8080/"
